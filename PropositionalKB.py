80e0b0cf415e17e3375cf4ce3810660eac8f02cf
Anushree <anushreerdesai@gmail.com>
2018-12-09 13:54:50 -0500

    Test tell kb

--- PropositionalKB.py
+++ PropositionalKB.py
@@ -3,7 +3,7 @@ from agent import BaseAgent
 from sympy import *
 from sympy.logic.boolalg import And, Not, conjuncts, to_cnf
 
-class PropKB:
+class PropsitionalKB:
     def __init__(self, sentence = None):
         self.clauses = []
         if sentence:
@@ -15,8 +15,21 @@ class PropKB:
     def tellPhysics(self, game_map, map_objects):
         self.game_map = game_map
         self.map_objects = map_objects
+    
+    def getKB(self):
+        return self.clauses
 
     # def ask(self, query):
     #     checkClause = KBentailsAlpha(self.clauses, query)
     #     if (checkClause == {}): return True
     #     else: return False
+
+propkb = PropsitionalKB()
+a,b = symbols('a,b')
+propkb.tell(Implies(a,b))
+print('here is my KB')
+print(propkb.getKB())
+
+propkb.tell(symbols('p'))
+print('now my kb is: ')
+print(propkb.getKB())
